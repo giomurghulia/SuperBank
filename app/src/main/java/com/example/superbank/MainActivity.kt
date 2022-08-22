aed5146b7e66828859924673c3c9720bc495a283
giomurghulia <48592227+giomurghulia@users.noreply.github.com>
2022-08-22 20:58:50 +0400

    Initial commit with bottom nav bar

---
+++ app/src/main/java/com/example/superbank/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.superbank
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.example.superbank.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.activity_main_nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        setupWithNavController(binding.bottomNavigationView, navController)
+    }
+
+}
---
+++ app/src/main/java/com/example/superbank/cards/MyCardsViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.superbank.cards
+
+import androidx.lifecycle.ViewModel
+
+class MyCardsViewModel : ViewModel() {
+
+
+}
