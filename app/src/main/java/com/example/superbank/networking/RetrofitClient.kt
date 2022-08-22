ae3f34ec36bcf332c1147a75f7b77a268fc0a21b
giomurghulia <48592227+giomurghulia@users.noreply.github.com>
2022-08-22 21:40:28 +0400

    add network layer

---
+++ app/src/main/java/com/example/superbank/networking/RetrofitClient.kt
@@ -0,0 +1,38 @@
+package com.example.superbank.networking
+
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.POST
+
+object RetrofitClient {
+    private const val BASE_URL = "https://run.mocky.io/v3/"
+
+    private val interceptor = HttpLoggingInterceptor().apply {
+        setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+    private val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
+
+    private val retrofitBuilder by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
+
+    val apiService by lazy {
+        retrofitBuilder.create(ApiService::class.java)
+    }
+}
+
+interface ApiService {
+    @POST("ab565edd-01b6-417e-bbdc-502c3b432cd9")
+    suspend fun getCards(): Response
+
+    @POST("91fc90ba-bebe-476a-8198-9389d9306671")
+    suspend fun getPayment(): Response
+}
