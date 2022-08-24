1d2cd6362edc55c34965a785920cf0fa73d9bb60
giomurghulia <48592227+giomurghulia@users.noreply.github.com>
2022-08-24 19:58:03 +0400

    add OkhttpClient with DefaultParams

--- app/src/main/java/com/example/superbank/networking/RetrofitClient.kt
+++ app/src/main/java/com/example/superbank/networking/RetrofitClient.kt
@@ -3,6 +3,7 @@ package com.example.superbank.networking
 import com.example.superbank.cards.Card
 import com.example.superbank.cards.CardTransactions
 import com.example.superbank.cards.UpcomingPayment
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Response
@@ -13,23 +14,43 @@ import retrofit2.http.Query
 
 object RetrofitClient {
     private const val BASE_URL = "https://run.mocky.io/v3/"
+    private const val TOKEN_KEY = "token"
+    var token: String? = "null"
 
-    private val interceptor = HttpLoggingInterceptor().apply {
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
         setLevel(HttpLoggingInterceptor.Level.BODY)
     }
-    private val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
 
 
     private val retrofitBuilder by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .client(client)
+            .client(createOkhttpClient())
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
     }
 
     val apiService by lazy {
         retrofitBuilder.create(ApiService::class.java)
+    }
+
+    private fun createOkhttpClient(): OkHttpClient {
+    val httpClient = OkHttpClient.Builder()
+        .addInterceptor(addDefaultParams())
+        .addInterceptor(loggingInterceptor)
+        return httpClient.build()
+    }
+
+    private fun addDefaultParams(): Interceptor {
+        return Interceptor { chain ->
+            val url = chain.request()
+                .url.newBuilder()
+                .addQueryParameter(TOKEN_KEY, token)
+                .build()
+
+            chain.proceed(chain.request().newBuilder().url(url).build())
+
+        }
     }
 }
 
