e468f836927e65ae6f505c3d258fd4a8caca9692
giomurghulia <48592227+giomurghulia@users.noreply.github.com>
2022-08-28 15:32:54 +0400

    update splashScreen

--- app/src/main/java/com/example/superbank/MainActivity.kt
+++ app/src/main/java/com/example/superbank/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
         installSplashScreen().apply {
             setKeepOnScreenCondition {
                 viewModel.isNotLoaded.value && Firebase.auth.currentUser != null
